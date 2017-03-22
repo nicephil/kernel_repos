44dd41f809b41210bb035dcd682116847dd0f497
Leilei Wang <leilei.wang@oakridge.io>
2017-03-22 12:28:49 +0800

    add DEV_NAME check to support all products

--- arch/mips/ath79/mach-ap152.c
+++ arch/mips/ath79/mach-ap152.c
@@ -46,25 +46,13 @@
 #define AP152_GPIO_LED_LAN3             14
 #define AP152_GPIO_LED_LAN4             11
 
-#if !OK_PATCH
 #define AP152_GPIO_LED_USB0		7
 #define AP152_GPIO_LED_USB1		8
-#else
-#define AP152_GPIO_LED_RED		6
-#define AP152_GPIO_LED_GREEN	7
-#define AP152_GPIO_LED_BLUE		8
-#endif /* OK_PATCH */
 
-#if !OK_PATCH
 #define AP152_GPIO_BTN_RESET            2
 #define AP152_GPIO_BTN_WPS              1
 #define AP152_KEYS_POLL_INTERVAL        20     /* msecs */
 #define AP152_KEYS_DEBOUNCE_INTERVAL    (3 * AP152_KEYS_POLL_INTERVAL)
-#else
-#define AP152_GPIO_BTN_RESET            2
-#define AP152_KEYS_POLL_INTERVAL        20     /* msecs */
-#define AP152_KEYS_DEBOUNCE_INTERVAL    (3 * AP152_KEYS_POLL_INTERVAL)
-#endif
 
 #define AP152_MAC0_OFFSET               0
 #define AP152_MAC1_OFFSET               6
@@ -74,7 +62,6 @@
 #define AP152_GPIO_MDIO			4
 
 static struct gpio_led ap152_leds_gpio[] __initdata = {
-#if !OK_PATCH
 	{
 		.name		= "ap152:green:usb0",
 		.gpio		= AP152_GPIO_LED_USB0,
@@ -85,27 +72,9 @@ static struct gpio_led ap152_leds_gpio[] __initdata = {
 		.gpio		= AP152_GPIO_LED_USB1,
 		.active_low	= 1,
 	},
-#else
-	{
-		.name		= "ap152:red:status",
-		.gpio		= AP152_GPIO_LED_RED,
-		.active_low	= 1,
-	},
-	{
-		.name		= "ap152:green:status",
-		.gpio		= AP152_GPIO_LED_GREEN,
-		.active_low	= 1,
-	},
-	{
-		.name		= "ap152:blue:status",
-		.gpio		= AP152_GPIO_LED_BLUE,
-		.active_low	= 1,
-	},
-#endif /* OK_PATCH */
 };
 
 static struct gpio_keys_button ap152_gpio_keys[] __initdata = {
-#if !OK_PATCH
         {
                 .desc           = "WPS button",
                 .type           = EV_KEY,
@@ -122,18 +91,113 @@ static struct gpio_keys_button ap152_gpio_keys[] __initdata = {
                 .gpio           = AP152_GPIO_BTN_RESET,
                 .active_low     = 1,
         },
-#else
+};
+
+
+#if OK_PATCH
+/* A820 */
+#define AP152_QTSA820_GPIO_LED_GREEN	1
+
+#define AP152_QTSA820_GPIO_BTN_RESET    2
+
+static struct gpio_led ap152_qtsa820_leds_gpio[] __initdata = {
+	{
+		.name		= "ap152:green:usb1",
+		.gpio		= AP152_QTSA820_GPIO_LED_GREEN,
+		.active_low	= 1,
+	},
+};
+
+static struct gpio_keys_button ap152_qtsa820_gpio_keys[] __initdata = {
         {
                 .desc           = "Reset button",
                 .type           = EV_KEY,
                 .code           = KEY_RESTART,
                 .debounce_interval = AP152_KEYS_DEBOUNCE_INTERVAL,
-                .gpio           = AP152_GPIO_BTN_RESET,
+                .gpio           = AP152_QTSA820_GPIO_BTN_RESET,
                 .active_low     = 1,
         },
-
-#endif
 };
+
+/* WL8200-T2 */
+#define AP152_WL8200T2_GPIO_LED_RED		6
+#define AP152_WL8200T2_GPIO_LED_GREEN	7
+#define AP152_WL8200T2_GPIO_LED_BLUE	8
+
+#define AP152_WL8200T2_GPIO_BTN_RESET   2
+
+static struct gpio_led ap152_wl8200t2_leds_gpio[] __initdata = {
+	{
+		.name		= "ap152:red:status",
+		.gpio		= AP152_WL8200T2_GPIO_LED_RED,
+		.active_low	= 1,
+	},
+	{
+		.name		= "ap152:green:status",
+		.gpio		= AP152_WL8200T2_GPIO_LED_GREEN,
+		.active_low	= 1,
+	},
+	{
+		.name		= "ap152:blue:status",
+		.gpio		= AP152_WL8200T2_GPIO_LED_BLUE,
+		.active_low	= 1,
+	},
+};
+
+static struct gpio_keys_button ap152_wl8200t2_gpio_keys[] __initdata = {
+        {
+                .desc           = "Reset button",
+                .type           = EV_KEY,
+                .code           = KEY_RESTART,
+                .debounce_interval = AP152_KEYS_DEBOUNCE_INTERVAL,
+                .gpio           = AP152_WL8200T2_GPIO_BTN_RESET,
+                .active_low     = 1,
+        },
+};
+
+/* WL8200R2 */
+
+#define AP152_WL8200R2_GPIO_LED_RED		6
+#define AP152_WL8200R2_GPIO_LED_GREEN	7
+#define AP152_WL8200R2_GPIO_LED_BLUE	8
+
+#define AP152_WL8200R2_GPIO_BTN_RESET   2
+
+
+static struct gpio_led ap152_wl8200r2_leds_gpio[] __initdata = {
+	{
+		.name		= "ap152:red:status",
+		.gpio		= AP152_WL8200R2_GPIO_LED_RED,
+		.active_low	= 1,
+	},
+	{
+		.name		= "ap152:green:status",
+		.gpio		= AP152_WL8200R2_GPIO_LED_GREEN,
+		.active_low	= 1,
+	},
+	{
+		.name		= "ap152:blue:status",
+		.gpio		= AP152_WL8200R2_GPIO_LED_BLUE,
+		.active_low	= 1,
+	},
+};
+
+static struct gpio_keys_button ap152_wl8200r2_gpio_keys[] __initdata = {
+        {
+                .desc           = "Reset button",
+                .type           = EV_KEY,
+                .code           = KEY_RESTART,
+                .debounce_interval = AP152_KEYS_DEBOUNCE_INTERVAL,
+                .gpio           = AP152_WL8200R2_GPIO_BTN_RESET,
+                .active_low     = 1,
+        },
+};
+
+
+#endif /* OK_PATCH */
+
+
+
 
 static struct ar8327_pad_cfg ap152_ar8337_pad0_cfg = {
 	.mode = AR8327_PAD_MAC_SGMII,
@@ -168,17 +232,77 @@ static void __init ap152_mdio_setup(void)
 	ath79_register_mdio(0, 0x0);
 }
 
+#if OK_PATCH
+#define AP152_EINFO_OFFSET 0x20
+#define AP152_EINFO_DEV_NAME_LEN 33
+#define AP152_EINFO_DEV_NAME_OFFSET 10
+static char ok_dev_name[AP152_EINFO_DEV_NAME_LEN] = {0};
+void __init ath79_init_dev_name(unsigned char *dst, const unsigned char *src)
+{
+    const unsigned char *ptr1;
+    const unsigned char *ptr2;
+    int size = 0;
+    ptr1 = ptr2 = src + AP152_EINFO_OFFSET + AP152_EINFO_DEV_NAME_OFFSET;
+    while (*ptr1++ != '\"');
+    size = ptr1 - ptr2;
+    size = (size < AP152_EINFO_DEV_NAME_LEN - 1) ? size : (AP152_EINFO_DEV_NAME_LEN - 1);
+    memcpy(dst, ptr2, size);
+    dst[size - 1]='\0';
+}
+#endif /* OK_PATCH */
+
 static void __init ap152_setup(void)
 {
 	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
 
+#if OK_PATCH
+    ath79_init_dev_name(ok_dev_name, art);
+    printk("-->OK_DEV_NAME=%s", ok_dev_name);
+#endif /* OK_PATCH */
+
 	ath79_register_m25p80(NULL);
+
+#if OK_PATCH
+/* supported device list */
+#define AP152_QTSA820_DEV_NAME        "A820"
+#define AP152_WL8200T2_DEV_NAME       "WL8200-T2"
+#define AP152_WL8200R2_DEV_NAME       "WL8200-R2"
+
+    if (!strcmp(ok_dev_name, AP152_QTSA820_DEV_NAME)) {
+        ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_qtsa820_leds_gpio),
+                ap152_qtsa820_leds_gpio);
+        ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
+                ARRAY_SIZE(ap152_qtsa820_gpio_keys),
+                ap152_qtsa820_gpio_keys);
+    } else if (!strcmp(ok_dev_name, AP152_WL8200T2_DEV_NAME)) {
+        ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_wl8200t2_leds_gpio),
+                ap152_wl8200t2_leds_gpio);
+        ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
+                ARRAY_SIZE(ap152_wl8200t2_gpio_keys),
+                ap152_wl8200t2_gpio_keys);
+    } else if (!strcmp(ok_dev_name, AP152_WL8200R2_DEV_NAME)) {
+        ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_wl8200r2_leds_gpio),
+                ap152_wl8200r2_leds_gpio);
+        ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
+                ARRAY_SIZE(ap152_wl8200r2_gpio_keys),
+                ap152_wl8200r2_gpio_keys);
+    } else {
+        printk("WARN: %s using default GPIO Setting.", ok_dev_name);
+        ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_leds_gpio),
+                ap152_leds_gpio);
+        ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
+                ARRAY_SIZE(ap152_gpio_keys),
+                ap152_gpio_keys);
+    }
+
+#else
 
 	ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_leds_gpio),
 			ap152_leds_gpio);
         ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
                                         ARRAY_SIZE(ap152_gpio_keys),
                                         ap152_gpio_keys);
+#endif /* OK_PATCH */
 
 	ath79_register_usb();
 
