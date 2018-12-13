0117b33d582bf00f55a9f7d87ee3adcc07efa5e7
Leilei Wang <leilei.wang@oakridge.io>
2018-12-13 15:23:27 +0800

    add A751 support

--- arch/mips/ath79/mach-ap152.c
+++ arch/mips/ath79/mach-ap152.c
@@ -332,6 +332,7 @@ static void __init ap152_setup(void)
 #define AP152_WL8200I2_DEV_NAME       "WL8200-I2"
 #define AP152_QTSW282_DEV_NAME        "W282"
 #define AP152_OKA750_DEV_NAME         "A750"
+#define AP152_OKA751_DEV_NAME         "A751"
 
     if (!strcmp(ok_dev_name, AP152_QTSA820_DEV_NAME) || !strcmp(ok_dev_name, AP152_QTSA822_DEV_NAME) || !strcmp(ok_dev_name, AP152_QTSA826_DEV_NAME)) {
         ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_qtsa820_leds_gpio),
@@ -399,7 +400,7 @@ static void __init ap152_setup(void)
 
         return;
 
-    } else if (!strcmp(ok_dev_name, AP152_OKA750_DEV_NAME)) {
+    } else if (!strcmp(ok_dev_name, AP152_OKA750_DEV_NAME) || !strcmp(ok_dev_name, AP152_OKA751_DEV_NAME)) {
         ath79_register_leds_gpio(-1, ARRAY_SIZE(ap152_oka750_leds_gpio),
                 ap152_oka750_leds_gpio);
         ath79_register_gpio_keys_polled(-1, AP152_KEYS_POLL_INTERVAL,
